d238fce1bbd7d4afb4419f944e8d37cf9edf600f
Sayem Hossain <sayemkcn@gmail.com>
2021-02-08 22:20:30 +0600

    [Chane] public api versioin

--- src/main/java/com/example/auth/config/security/oauth/ResourceServerConfig.java
+++ src/main/java/com/example/auth/config/security/oauth/ResourceServerConfig.java
@@ -49,7 +49,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         "/api/v1/promos/**",
                         "/swagger-ui.html",
                         "/api/v1/profiles/user/snap/*",
-                        "/api/v2/public/**"
+                        "/api/v1/public/**"
 
                 )
                 .permitAll()
